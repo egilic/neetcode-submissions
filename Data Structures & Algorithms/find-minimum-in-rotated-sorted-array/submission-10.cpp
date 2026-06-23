a5232fede2a6ca1464eecbd54d74ad0acbaaa3de
edin <97710816+egilic@users.noreply.github.com>
2026-06-23 13:03:43 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-10

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-10.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1, m, min = INT_MAX;
+
+        // First find deflection point where element to the left is greater and element to the right is greater
+        while (l < r)
+        {
+            m = (l + r) / 2;
+
+            if (nums[m] > nums[r])
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m;
+            }
+        }
+        return nums[l];
+    }
+};
+
+// 4 5 0 
+// 
