c59067d1c4dd412ea09f603d68c0eedf5d08b0b6
edin <97710816+egilic@users.noreply.github.com>
2026-08-12 17:25:39 -0400

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) { return 1; }
+        if (n == 2) { return 2; }
+        
+        // Bottom up
+        vector<int> cache(n);
+        cache[0] = 1;
+        cache[1] = 2;
+
+        for (int i = 2; i < n; ++i)
+        {
+            cache[i] = cache[i-1] + cache[i-2];
+        }
+
+        return cache[n-1];
+    }
+};
